aef1957275d5e8b1817f38258fb47483497c02c1
wutsi <herve.tchepannou@wutsi.com>
2022-09-08 07:16:21 -0400

    redirect to onboard page

--- src/main/kotlin/com/wutsi/application/shell/endpoint/settings/security/command/DeleteWallet.kt
+++ src/main/kotlin/com/wutsi/application/shell/endpoint/settings/security/command/DeleteWallet.kt
@@ -3,7 +3,9 @@ package com.wutsi.application.shell.endpoint.settings.security.command
 import com.wutsi.application.shell.endpoint.AbstractCommand
 import com.wutsi.application.shell.endpoint.settings.logout.command.LogoutCommand
 import com.wutsi.flutter.sdui.Action
+import com.wutsi.flutter.sdui.enums.ActionType
 import com.wutsi.platform.account.WutsiAccountApi
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,9 +16,23 @@ class DeleteWallet(
     private val accountApi: WutsiAccountApi,
     private val logout: LogoutCommand
 ) : AbstractCommand() {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(DeleteWallet::class.java)
+    }
+
     @PostMapping
     fun index(): Action {
         accountApi.suspendAccount(securityContext.currentAccountId())
-        return logout.index()
+
+        try {
+            logout.index()
+        } catch (ex: Exception) {
+            LOGGER.warn("Unexpected error while login out")
+        } finally {
+            return Action(
+                type = ActionType.Route,
+                url = urlBuilder.build(loginUrl, "/onboard")
+            )
+        }
     }
 }
--- src/test/kotlin/com/wutsi/application/shell/endpoint/settings/security/command/DeleteWalletTest.kt
+++ src/test/kotlin/com/wutsi/application/shell/endpoint/settings/security/command/DeleteWalletTest.kt
@@ -1,11 +1,10 @@
 package com.wutsi.application.shell.endpoint.settings.security.command
 
-import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.shell.endpoint.AbstractEndpointTest
 import com.wutsi.application.shell.endpoint.settings.logout.command.LogoutCommand
 import com.wutsi.flutter.sdui.Action
+import com.wutsi.flutter.sdui.enums.ActionType
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -32,9 +31,6 @@ internal class DeleteWalletTest : AbstractEndpointTest() {
 
     @Test
     fun invoke() {
-        val action = Action()
-        doReturn(action).whenever(logout).index()
-
         val response = rest.postForEntity(url, null, Action::class.java)
 
         // THEN
@@ -42,5 +38,9 @@ internal class DeleteWalletTest : AbstractEndpointTest() {
 
         verify(accountApi).suspendAccount(ACCOUNT_ID)
         verify(logout).index()
+
+        val action = response.body!!
+        assertEquals(ActionType.Route, action.type)
+        assertEquals("https://wutsi-gateway-test.herokuapp.com/login/onboard", action.url)
     }
 }
