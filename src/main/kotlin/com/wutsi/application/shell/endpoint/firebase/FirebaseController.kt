0b8b72e98f86bc3406a4ba5cb24d67bea5cfe86a
wutsi <herve.tchepannou@wutsi.com>
2022-09-25 20:58:50 -0400

    update selection ahndler

--- src/main/kotlin/com/wutsi/application/shell/endpoint/firebase/FirebaseController.kt
+++ src/main/kotlin/com/wutsi/application/shell/endpoint/firebase/FirebaseController.kt
@@ -2,7 +2,6 @@ package com.wutsi.application.shell.endpoint.firebase
 
 import com.wutsi.application.shell.endpoint.AbstractQuery
 import com.wutsi.application.shell.endpoint.firebase.dto.HandleMessageRequest
-import com.wutsi.application.shell.endpoint.firebase.dto.HandleSelectionRequest
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,17 +14,21 @@ class FirebaseController : AbstractQuery() {
     fun onMessage(
         @RequestBody request: HandleMessageRequest
     ) {
+        log(request)
+    }
+
+    @PostMapping("/on-select")
+    fun onSelect(
+        @RequestBody request: HandleMessageRequest
+    ) {
+        log(request)
+    }
+
+    private fun log(request: HandleMessageRequest) {
         logger.add("message_title", request.title)
         logger.add("message_body", request.body)
         logger.add("message_image_url", request.imageUrl)
         logger.add("message_background", request.background)
         logger.add("message_data", request.data)
-    }
-
-    @PostMapping("/on-select")
-    fun onSelect(
-        @RequestBody request: HandleSelectionRequest
-    ) {
-        logger.add("payload", request.payload)
     }
 }
--- src/main/kotlin/com/wutsi/application/shell/endpoint/firebase/dto/HandleSelectionRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.wutsi.application.shell.endpoint.firebase.dto
-
-data class HandleSelectionRequest(
-    val payload: Map<String, String?>? = null
-)
