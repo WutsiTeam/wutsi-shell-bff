52d9b8d53159c60ff6cd12b6d1b40a999d3add01
wutsi <herve.tchepannou@wutsi.com>
2022-11-21 13:45:17 -0500

    edit product and images

---
+++ src/test/kotlin/com/wutsi/application/marketplace/settings/catalog/picture/screen/SettingsV2ProductPictureScreenTest.kt
@@ -0,0 +1,16 @@
+package com.wutsi.application.marketplace.settings.catalog.picture.screen
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SettingsV2ProductPictureScreenTest {
+
+    @Test
+    fun index() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
