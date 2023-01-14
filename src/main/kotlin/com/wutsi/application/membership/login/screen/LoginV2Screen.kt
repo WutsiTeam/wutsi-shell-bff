85b6f22de10a9241ea35ea2a2d76137310f4ec41
wutsi <herve.tchepannou@wutsi.com>
2023-01-14 16:39:40 -0500

    fix lint issues

--- src/main/kotlin/com/wutsi/application/membership/login/screen/LoginV2Screen.kt
+++ src/main/kotlin/com/wutsi/application/membership/login/screen/LoginV2Screen.kt
@@ -179,8 +179,7 @@ class LoginV2Screen(
                             } else {
                                 null
                             },
-
-                            ),
+                        ),
                     ),
                 ),
             ),
